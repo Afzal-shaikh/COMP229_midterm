fcf41d5852e17c0658a9f1626784edc235556dad
Afzal-shaikh <shaikhafzal9900@gmail.com>
2021-10-28 19:23:57 -0400

    books details and index changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
   "URI": "mongodb://localhost/books229"
+  // "URI": "mongodb+srv://afzal:afzal5136@cluster0.qvroo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 };
