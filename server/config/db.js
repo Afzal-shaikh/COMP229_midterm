3837740808496eec14a926aed96a163700303e37
Afzal-shaikh <shaikhafzal9900@gmail.com>
2021-11-08 00:56:43 -0500

    added new URI route

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
   // "URI": "mongodb://localhost/books229"
-  "URI": "mongodb+srv://afzal:afzal5136@cluster0.qvroo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  // "URI": "mongodb+srv://afzal:afzal5136@cluster0.qvroo.mongodb.net/books229?retryWrites=true&w=majority"
+
+  "URI": "mongodb+srv://afzal:afzal5136@cluster0.qvroo.mongodb.net/books229?retryWrites=true&w=majority"
  };
